ff1605e1fc72a7121b0abd45ed91253e39b1548e
Ravi Kumar K G <ravi.rk2406@gmail.com>
2021-05-31 18:58:38 +0000

    rabbitmq changes

--- src/main/java/com/covid19army/UserService/UserServiceApplication.java
+++ src/main/java/com/covid19army/UserService/UserServiceApplication.java
@@ -1,11 +1,14 @@
 package com.covid19army.UserService;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+
+import com.covid19army.core.mex.rabbitmq.RabbitMQSender;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -19,6 +22,14 @@ public class UserServiceApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 	    return new ModelMapper();
+	}
+	
+	@Bean
+	public RabbitMQSender otpExchangeSender(
+			@Value("${covid19army.rabbitmq.mobileotpexchange}") final String otpexchange,
+			@Value("${covid19army.rabbitmq.mobileotpexchange.routingkey:}") final String routingkey) {
+		return new RabbitMQSender(otpexchange, routingkey);
+		
 	}
 
 }
--- src/main/java/com/covid19army/UserService/services/UserService.java
+++ src/main/java/com/covid19army/UserService/services/UserService.java
@@ -1,10 +1,16 @@
 package com.covid19army.UserService.services;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.covid19army.UserService.models.User;
 import com.covid19army.UserService.repositories.UserRepository;
+import com.covid19army.core.dtos.MobileVerificationQueueDto;
+import com.covid19army.core.dtos.OtpVerificationRequestDto;
+import com.covid19army.core.mex.rabbitmq.RabbitMQSender;
+
 import java.util.Optional;
 
 @Service
@@ -12,6 +18,13 @@ public class UserService {
 
 	@Autowired
 	UserRepository _userRepository;
+	
+	@Autowired
+	ModelMapper _mapper;
+	
+	@Autowired
+	@Qualifier("otpExchangeSender")
+	RabbitMQSender _otpExchangeSender;
 	
 	public boolean userExists(String mobileNumber) {
 		return _userRepository.findByMobilenumber(mobileNumber) != null;
@@ -23,15 +36,24 @@ public class UserService {
 	
 	public User login(String mobileNumber, String clientIp) {
 		Optional<User> optionalUser = this.getUserByMobileNumber(mobileNumber);
-		User user = null;
-		if(!optionalUser.isPresent()) {
+		User user = null ;
+		if(optionalUser.isEmpty()) {
 			user = new User();
 			user.setMobilenumber(mobileNumber);
 			user.setClientip(clientIp);
 			
 			user = _userRepository.save(user);			
+		}else {
+			user = optionalUser.get();
 		}
 		
-		return optionalUser.orElse(user);
+		MobileVerificationQueueDto otpdto = new MobileVerificationQueueDto();
+		otpdto.setMobilenumber(user.getMobilenumber());
+		otpdto.setEntityid(user.getUserid());
+		otpdto.setEntitytype("USR");
+		
+		_otpExchangeSender.<MobileVerificationQueueDto>send(otpdto);
+		
+		return user;
 	}
 }
